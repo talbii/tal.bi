b2c70123ebe160cd4ff0f6bf1073d5b627580516
talbii <talbioffical@gmail.com>
2021-10-03 22:24:34 +0300

    Updated blog page: WIP.

--- nav.js
+++ nav.js
@@ -1,6 +1,6 @@
 document.write(" \
     <a href='/'> home </a> \
-    <a href='/blog/'> blog </a> \
+    <a> blog <sup>WIP</sup> </a> \
     <a href='/about.html'> about </a> \
     <a href='/contact.html'> contact </a> \
 ");
