4acfc12a5f803c684873dde891add15d234737a2
talbii <id28102004@gmail.com>
2021-10-16 21:54:12 +0000

    Blog is out!

--- nav.js
+++ nav.js
@@ -1,6 +1,6 @@
 document.write(" \
     <a href='/'> home </a> \
-    <a> blog <sup>WIP</sup> </a> \
+    <a href='blog.tal.bi'> blog </a> \
     <a href='/about.html'> about </a> \
     <a href='/contact.html'> contact </a> \
 ");
