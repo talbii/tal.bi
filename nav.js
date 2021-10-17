429adb2c91ba2eb4b11b4886d88f5bfb80a8fdd6
talbii <41526934+talbii@users.noreply.github.com>
2021-10-17 08:48:30 +0300

    Fixed link to blog

--- nav.js
+++ nav.js
@@ -1,6 +1,6 @@
 document.write(" \
     <a href='/'> home </a> \
-    <a href='blog.tal.bi'> blog </a> \
+    <a href='https://blog.tal.bi'> blog </a> \
     <a href='/about.html'> about </a> \
     <a href='/contact.html'> contact </a> \
 ");
