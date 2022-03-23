32ae104946d510cf2ccb69ea626c22ae62b145dc
talbii <ido@tal.bi>
2022-03-23 21:21:27 +0200

    Temp commit, please ignore :)

---
+++ physics/_next/static/mgtI4WFWrdros1efQ4YDV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/171-790d7d668343e840.js","static/chunks/pages/index-ea522d0dbbfafec2.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
