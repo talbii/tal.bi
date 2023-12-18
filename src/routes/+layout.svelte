43cb3b806dcf050547b0e20f396a09660ce7542e
talbii <ido@tal.bi>
2023-12-18 22:15:34 +0200

    Move `Navbar` to its own component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,38 +1,8 @@
 <script>
-import '$src/app.css'
+    import '$src/app.css'
+    import Navbar from '$lib/Navbar.svelte'
 </script>
 
-<nav> 
-    <h1> Ido Talbi </h1>
-    <div> 
-        <a href="/">home</a>
-        <a href="/blog">blog</a>
-        <a href="/contact.html">contact</a>
-    </div>
-</nav>
+<Navbar />
 
 <slot/>
-
-<style>
-nav {
-    border-bottom: 2px solid black; 
-    padding: 5px 0;
-/*    padding-bottom: 0; */
-    margin: 5px 0;
-    display: flex;
-    flex-direction: row;
-    align-items: flex-end;
-}
-
-nav > h1 {
-    font-family: 'Alegreya', serif;
-    font-size: 2rem;
-    display: inline-block;
-    flex: 1;
-}
-
-nav > * a {
-    text-decoration: none;
-}
-</style>
-
