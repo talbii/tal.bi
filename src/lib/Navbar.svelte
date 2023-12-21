85e41cd097ab6816fd94b054be00d5a5f0cf724f
talbii <ido@tal.bi>
2023-12-22 01:59:36 +0200

    Stylize hovering navbar links

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,5 +1,5 @@
 <nav> 
-    <a href="/"> <h1> Ido Talbi </h1> </a>
+    <a class='head-a' href="/"> <h1> Ido Talbi </h1> </a>
     <div style="flex: 1;"> </div>
     <div> 
         <a href="/">home</a>
@@ -10,7 +10,7 @@
 
 <style>
 nav {
-    border-bottom: 2px solid black; 
+    border-bottom: 2px solid var(--border-color);
     padding: 5px 0;
 /*    padding-bottom: 0; */
     margin: 5px 0;
@@ -19,15 +19,32 @@ nav {
     align-items: flex-end;
 }
 
-nav > a {
-    font-family: 'Alegreya', serif;
-    font-size: 1.8rem;
+a {
     display: inline-block;
     text-decoration: none;
+    color: unset;
 }
 
-nav > * a {
-    text-decoration: none;
+.head-a {
+    font-family: 'Alegreya', serif;
+    font-size: 1.8rem;
+    line-height: 1;
+}
+
+.head-a:hover {
+    color: var(--text);
+    text-decoration: underline;
+}
+
+a:hover {
+    color: var(--color-hover);
+}
+
+nav > div {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    gap: 5px;
 }
 </style>
 
