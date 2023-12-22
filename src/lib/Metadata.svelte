951d92f7d61bf1e7ffe53698d7d53aec3c3f6663
talbii <ido@tal.bi>
2023-12-22 02:02:18 +0200

    Introduce basic `Metadata` component

---
+++ src/lib/Metadata.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let title = undefined;
+    /* TODO https://ogp.me */
+
+    const title_ = title ? `${title} - ` : '';
+</script>
+
+<svelte:head>
+    <title> {title_} Ido Talbi </title>
+</svelte:head>
+
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <script>
     import PostSummary from '$lib/PostSummary.svelte'
+    import Metadata from '$src/lib/Metadata.svelte';
     export let data;
 </script>
+
+<Metadata />
 
 {#each data.posts as post}
     <PostSummary path={post.path} meta={post.meta} />
