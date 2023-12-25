78603548251a7e7c77293bd334f00217bf9e440a
talbii <ido@tal.bi>
2023-12-25 21:26:38 +0200

    Add monospace font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
     import '@fontsource-variable/source-sans-3';
+    import '@fontsource-variable/source-code-pro';
 
     import '$src/app.css'
     import Navbar from '$lib/Navbar.svelte'
