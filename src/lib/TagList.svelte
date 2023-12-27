da8d84d1dfa00163290f34cd78ea1288fb822814
talbii <ido@tal.bi>
2023-12-27 23:56:40 +0200

    Give some more padding for tags

--- src/lib/TagList.svelte
+++ src/lib/TagList.svelte
@@ -19,7 +19,7 @@ ul li {
     font-size: 0.8rem;
     display: inline;
     margin-right: 5px;
-    padding: 1px 3px;
+    padding: 1px 8px;
     border: 1px solid var(--border-color);
     border-radius: 0.25rem;
 }
