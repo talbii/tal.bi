2f426c6d7e2f3cf643525b1e7d6be7c294bf4830
talbii <ido@tal.bi>
2024-02-19 02:24:42 +0200

    mvp "copy codeblock" driver code
    doesn't actually copy anything but thats the idea.
    also very hacky, would like to replace as rehype plugin.

---
+++ src/lib/CodeblockCopyButton.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
+    import { faCopy } from "@fortawesome/free-regular-svg-icons";
+
+    export let node;
+    const text = node.textContent;
+</script>
+
+<button on:click={() => console.log(`copy: ${text}`)}>
+    <FontAwesomeIcon icon={faCopy}/>
+</button>
