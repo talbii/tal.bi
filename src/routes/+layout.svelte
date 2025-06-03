e9c6a0696118e1a13a1d501d012c96b4ea2b42f2
talbii <ido@tal.bi>
2025-06-03 23:13:13 +0300

    Revert "mvp "copy codeblock" driver code"
    This reverts commit 2f426c6d7e2f3cf643525b1e7d6be7c294bf4830.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,6 @@
 <script>
     import '$src/app.css'
     import Navbar from '$lib/Navbar.svelte'
-    import CodeblockCopyButton from '$lib/CodeblockCopyButton.svelte';
-    import { afterNavigate } from '$app/navigation';
-
-    afterNavigate(() => {
-        for (const node of document.querySelectorAll("pre > code")) {
-            new CodeblockCopyButton({
-                target: node,
-                props: {node},
-            })
-        }
-    });
 </script>
 
 <header>
